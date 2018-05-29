bef8bbd603c19ee02bda39161704b73b34a71260
Jiaqi Duan <imdjq1990@gmail.com>
2018-05-28 23:50:38 -0700

    testing app on Heroku

--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //connect to MongoDB
 mongoose.Promise = global.Promise;
 
+console.log("*****************************************"+process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
